a80264f3de45efbeb69192dcb1ef5308edb0b4a9
LeonPatmore <leon.patmore@vonage.com>
2020-08-14 22:07:28 +0100

    first commit

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+// Vue.component('button-counter', {
+//   data: function () {
+//     return {
+//       count: 0
+//     }
+//   },
+//   template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+// })
+
+<template>
+    <button ref="mainButton" v-on:click="count++">You clicked me {{ count }} times.</button>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            count: 0
+        }
+    },
+    name: 'myButton'
+}
+</script>
+
+<style>
+
+</style>
