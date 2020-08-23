84b04be5a9a0262c5862767dfa45967e3d95b72e
LeonPatmore <leon.patmore@vonage.com>
2020-08-23 01:18:09 +0100

    New: trying to setup vuetify.

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app dark>
+      <v-list dense>
+        <v-list-item to="/" link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/clients" link>
+          <v-list-item-action>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Clients</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Spammer V2</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      count: 0,
+      drawer: null,
+    };
+  },
+  name: "app",
+};
+</script>
+
+<style></style>
