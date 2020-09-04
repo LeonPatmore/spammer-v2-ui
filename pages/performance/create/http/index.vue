f3a8c49bad0df6ca32423d8d874c122232310109
LeonPatmore <leon.patmore@vonage.com>
2020-09-04 23:36:22 +0100

    Add v select

--- pages/performance/create/http/index.vue
+++ pages/performance/create/http/index.vue
@@ -4,9 +4,9 @@
       <h1>HTTP</h1>
       <v-form ref="form" lazy-validation>
         <v-text-field v-model="runtime" label="Runtime" required />
-        <v-text-field label="RPS" required />
-        <v-text-field label="URL" required />
-        <v-text-field label="Method" required />
+        <v-text-field v-model="rps" label="RPS" required />
+        <v-text-field v-model="url" label="URL" required />
+        <v-select v-model="method" :items="methods"></v-select>
         <v-btn small rounded @click="startTest">Start</v-btn>
       </v-form>
     </v-container>
@@ -18,6 +18,10 @@ export default {
   data() {
     return {
       runtime: 0,
+      rps: 0,
+      url: "",
+      method: "POST",
+      methods: ["POST", "GET"],
     };
   },
   methods: {
